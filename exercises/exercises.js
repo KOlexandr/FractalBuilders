09051a04e0753b2c9a5df5c023384791dbffbd98
Olexandr <kolexandr.13@gmail.com>
2015-04-01 00:21:13 +0300

    exercises

---
+++ exercises/exercises.js
@@ -0,0 +1,36 @@
+function ñantorFunction(n) {
+    // calculate function of Ñantor
+    // n must be in [0,1] interval
+    if (n < 0 || n > 1) {
+        throw new Error('Invalid input argument');
+    } else if (n == 0 || n == 1) {
+        return n;
+    } else {
+        return step(0, 1, n, 0.5);
+    }
+}
+
+function step(left, right, arg, ordinate) {
+    var partLen = (right - left) / 3;
+    if ((arg >= left + partLen) && (arg <= right - partLen)) {
+        return ordinate;
+    } else if (arg < (right + left) / 2) {
+        return step(left, left + partLen, arg, ordinate - ordinate / 2);
+    } else {
+        return step(right - partLen, right, arg, ordinate + ordinate / 2);
+    }
+}
+
+function showCantor(num, denom) {
+    return "Cantor function f(" + num + "/" + denom + ") = " + ñantorFunction(num/denom)
+}
+
+function test(){
+    document.getElementById("ex1").innerHTML = showCantor(7, 8);
+    document.getElementById("ex2").innerHTML = showCantor(6, 23);
+    document.getElementById("ex5").innerHTML = showCantor(7, 65);
+
+    new Exercise16("ex6").build([4]);
+    new Exercise16("ex7").build([4, 6]);
+    new Exercise16("ex8").build([3, 7]);
+}
